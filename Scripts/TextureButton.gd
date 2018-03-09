2a36ff826a16946b06d2e0a0fdd678eef3a5a72d
Yaboy <yaboy657@yahoo.com>
2018-03-09 18:26:28 -0500

    B2
    xd

--- Scripts/TextureButton.gd
+++ Scripts/TextureButton.gd
@@ -1,21 +1,11 @@
 extends TextureButton
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-
-func _on_F_gangster_enable():
-	set_disabled(false)
-	print("Gagster enabled")
+func _on_F_gangster_enable(gangster_enabled):
+	if(gangster_enabled == true):
+		set_disabled(false)
+		print("Gangster is enabled")
+	else:
+		print("Gangster is disabled")
